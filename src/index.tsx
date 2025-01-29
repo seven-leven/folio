1ed8f033bb5e2c0dcc4e6d350d1886776dabf6b9
Your Name <you@example.com>
2025-01-29 16:10:12 +0500

    a

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/folio">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
